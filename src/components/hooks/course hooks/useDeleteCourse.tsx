513b427ae550dbb99780faf48addf7a1414b7a2f
Dencel Cosme <miguithegreat@gmail.com>
2024-05-28 21:55:55 +0800

    Added delete

---
+++ src/components/hooks/course hooks/useDeleteCourse.tsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Course from "../../interfaces/Course";
+import useAuthContext from "../useAuthContext";
+import swal from "sweetalert";
+import { toastNotify } from "../../helpers/toastNotify";
+
+const useDeleteCourse = (
+  course: Course | null,
+  setCourse: (course: Course) => void
+) => {
+  const [isPublishing, setIsPublishing] = useState(false);
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const updatePublishedStatus = async (
+    courseId: string | undefined,
+    newStatus: boolean | undefined
+  ) => {
+    try {
+      const response = await fetch(
+        `${
+          import.meta.env.VITE_REACT_APP_API_ROOT
+        }/api/course/publish/${courseId}`,
+        {
+          method: "PATCH",
+          headers: {
+            Authorization: `Bearer ${user.jwt}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ published: newStatus }),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("");
+      }
+
+      swal({
+        icon: "success",
+        text: course?.published
+          ? "Unpublished Successfully!"
+          : "Published Successfully!",
+      });
+      return true;
+    } catch (error) {
+      swal({
+        icon: "error",
+        text: String(error + ": Unauthorized"),
+      });
+      return false;
+    }
+  };
+
+  const handlePublishClick = async () => {
+    setIsPublishing(true);
+    const newStatus = !course?.published;
+    const success = await updatePublishedStatus(course?._id, newStatus);
+    if (success && course) {
+      setCourse({ ...course, published: newStatus });
+    }
+    setIsPublishing(false);
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_REACT_APP_API_ROOT}/api/course/${course?._id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${user.jwt}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Unauthorized");
+      }
+
+      toastNotify("Deleted Successfully!");
+      navigate("/courses");
+    } catch (error) {
+      console.error("Error deleting course:", error);
+      swal({
+        icon: "error",
+        text: String(error),
+      });
+    }
+  };
+
+  const confirmDelete = () => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this course!",
+      icon: "warning",
+      buttons: ["Cancel", "Delete"],
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        handleDelete();
+      }
+    });
+  };
+  return { isPublishing, handlePublishClick, confirmDelete };
+};
+
+export default useDeleteCourse;
