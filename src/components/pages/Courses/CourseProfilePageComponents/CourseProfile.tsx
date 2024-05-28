3619a6ca224e3675a42738aa20be0d4dda6bb8fa
Dencel Cosme <miguithegreat@gmail.com>
2024-05-28 21:56:53 +0800

    Implemented hook in courseprofile

--- src/components/pages/Courses/CourseProfilePageComponents/CourseProfile.tsx
+++ src/components/pages/Courses/CourseProfilePageComponents/CourseProfile.tsx
@@ -1,9 +1,5 @@
-import { useState } from "react";
-import swal from "sweetalert";
-import useAuthContext from "../../../hooks/useAuthContext";
-import { useNavigate } from "react-router-dom";
-import { toastNotify } from "../../../helpers/toastNotify";
 import Course from "../../../interfaces/Course";
+import useDeleteCourse from "../../../hooks/course hooks/useDeleteCourse";
 
 interface CourseProp {
   course: Course | null;
@@ -14,107 +10,10 @@ const CourseProfile: React.FC<CourseProp> = ({
   course,
   setCourse,
 }: CourseProp) => {
-  const [isPublishing, setIsPublishing] = useState(false);
-  const { user } = useAuthContext();
-  const navigate = useNavigate();
-
-  const handleClick = async () => {
-    console.log(isPublishing);
-    setIsPublishing(true);
-
-    const newStatus = !course?.published;
-    const success = await updatePublishedStatus(course?._id, newStatus);
-    if (success && course) {
-      setCourse({ ...course, published: newStatus });
-    }
-    setIsPublishing(false);
-  };
-
-  const updatePublishedStatus = async (
-    courseId: string | undefined,
-    newStatus: boolean | undefined
-  ) => {
-    try {
-      const response = await fetch(
-        `${
-          import.meta.env.VITE_REACT_APP_API_ROOT
-        }/api/course/publish/${courseId}`,
-        {
-          method: "PATCH",
-          headers: {
-            Authorization: `Bearer ${user.jwt}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ published: newStatus }),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to update published status");
-      }
-      if (course?.published) {
-        swal({
-          icon: "success",
-          text: "Unpublished Successfully!",
-        });
-      } else {
-        swal({
-          icon: "success",
-          text: "Published Successfully!",
-        });
-      }
-
-      return true;
-    } catch (error) {
-      swal({
-        icon: "error",
-        text: String(error + ": Unauthorized"),
-      });
-      return false;
-    }
-  };
-
-  const handleDelete = async () => {
-    try {
-      const response = await fetch(
-        `${import.meta.env.VITE_REACT_APP_API_ROOT}/api/course/${course?._id}`,
-        {
-          method: "DELETE",
-          headers: {
-            Authorization: `Bearer ${user.jwt}`,
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Unauthorized");
-      }
-
-      toastNotify("Deleted Successfully!");
-      navigate("/courses");
-    } catch (error) {
-      console.error("Error deleting course:", error);
-      swal({
-        icon: "error",
-        text: String(error),
-      });
-    }
-  };
-
-  const confirmDelete = () => {
-    swal({
-      title: "Are you sure?",
-      text: "Once deleted, you will not be able to recover this course!",
-      icon: "warning",
-      buttons: ["Cancel", "Delete"],
-      dangerMode: true,
-    }).then((willDelete) => {
-      if (willDelete) {
-        handleDelete();
-      }
-    });
-  };
-
+  const { isPublishing, handlePublishClick, confirmDelete } = useDeleteCourse(
+    course,
+    setCourse
+  );
   return (
     <>
       <div className="text-4xl breadcrumbs font-garetheavy text-theme-blue m-4">
@@ -139,7 +38,7 @@ const CourseProfile: React.FC<CourseProp> = ({
       </div>
       <div className="flex flex-row">
         <button
-          onClick={handleClick}
+          onClick={handlePublishClick}
           className={`btn ${
             course?.published ? "btn-secondary" : "bg-yellow-300"
           } font-garet m-4`}
