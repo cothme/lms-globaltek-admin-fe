283ed00740d2c3e5395e6b5d476566ac3086a4f6
Dencel Cosme <miguithegreat@gmail.com>
2024-06-25 04:38:12 +0800

    refactor: Add search functionality to CourseTable component

--- src/components/pages/Courses/CoursePageComponents/CourseTable.tsx
+++ src/components/pages/Courses/CoursePageComponents/CourseTable.tsx
@@ -1,10 +1,32 @@
+import { ImSearch } from "react-icons/im";
 import { useFetchAllCourse } from "../../../hooks/course hooks/useFetchAllCourse";
+import { useState } from "react";
 
 const CourseTable = () => {
   const { courses } = useFetchAllCourse();
+  const [courseSearch, setCourseSearch] = useState("");
+
+  const filteredCourses = courses.filter(
+    (course) =>
+      course.course_code?.toLowerCase().includes(courseSearch.toLowerCase()) ||
+      course.course_title?.toLowerCase().includes(courseSearch.toLowerCase()) ||
+      course.publisher?.toLowerCase().includes(courseSearch.toLowerCase())
+  );
 
   return (
     <>
+      <div className="relative m-4">
+        <input
+          type="text"
+          className="w-full inline-block border border-gray-300 rounded-lg px-4 py-2 pl-10 focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
+          placeholder="Search"
+          value={courseSearch}
+          onChange={(e) => setCourseSearch(e.target.value)}
+        />
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <ImSearch className="text-gray-400" />
+        </div>
+      </div>
       <div className="overflow-x-auto">
         <table className="table">
           {/* head */}
@@ -21,7 +43,7 @@ const CourseTable = () => {
           </thead>
           <tbody>
             {/* row 1 */}
-            {courses.map((course) => (
+            {filteredCourses.map((course) => (
               <tr key={course._id}>
                 <td>{course.course_code}</td>
                 <td>{course.course_title}</td>
--- src/components/pages/Courses/CoursesPage.tsx
+++ src/components/pages/Courses/CoursesPage.tsx
@@ -5,18 +5,7 @@ const CoursesPage = () => {
   return (
     <>
       <div className="flex flex-row w-full">
-        <div className="flex-grow mx-4">
-          <div className="relative">
-            <input
-              type="text"
-              className="w-full inline-block border border-gray-300 rounded-lg px-4 py-2 pl-10 focus:outline-none focus:border-accent focus:ring-1 focus:ring-accent"
-              placeholder="Search"
-            />
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <ImSearch className="text-gray-400" />
-            </div>
-          </div>
-        </div>
+        <div className="flex-grow mx-4"></div>
         <div className="mx-4">
           <a
             data-testid={`add-course`}
