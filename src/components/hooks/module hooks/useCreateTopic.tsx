281774c2d897fb8196bedc02b75bc70b8513817d
cothme <miguithegreat@gmail.com>
2024-09-24 17:04:15 +0800

    Fix build errors

--- src/components/hooks/module hooks/useCreateTopic.tsx
+++ src/components/hooks/module hooks/useCreateTopic.tsx
@@ -1,133 +1,132 @@
 import React, { useState } from "react";
-import { toastNotify } from "../../helpers/toastNotify";
 import useAuthContext from "../useAuthContext";
 import Topic from "../../interfaces/Topic";
 import { useParams } from "react-router-dom";
 
 const useCreateTopic = () => {
-  const { user } = useAuthContext();
-  const { courseId } = useParams();
-  const [formData, setFormData] = useState<Topic>({
-    topic_title: "",
-    topic_description: "",
-    pdf: "",
-    video: "",
-  });
-  const [pdfFile, setPdfFile] = useState<File | null>(null);
-  const [videoFile, setVideoFile] = useState<File | null>(null);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-  const [errors, setErrors] = useState<Partial<Topic>>({});
-  const [success, setSuccess] = useState<boolean>(false);
+	const { user } = useAuthContext();
+	const { courseId } = useParams();
+	const [formData, setFormData] = useState<Topic>({
+		topic_title: "",
+		topic_description: "",
+		pdf: "",
+		video: "",
+	});
+	const [pdfFile, setPdfFile] = useState<File | null>(null);
+	const [videoFile, setVideoFile] = useState<File | null>(null);
+	const [loading, setLoading] = useState<boolean>(false);
+	const [error, setError] = useState<string | null>(null);
+	const [errors, setErrors] = useState<Partial<Topic>>({});
+	const [success, setSuccess] = useState<boolean>(false);
 
-  const createTopic = async () => {
-    const currentErrors: Partial<Topic> = {};
+	const createTopic = async () => {
+		const currentErrors: Partial<Topic> = {};
 
-    if (!formData.topic_title)
-      currentErrors.topic_title = "Topic title is required";
+		if (!formData.topic_title)
+			currentErrors.topic_title = "Topic title is required";
 
-    if (Object.keys(currentErrors).length > 0) {
-      setErrors(currentErrors);
-      return { success: false, errors: currentErrors };
-    }
+		if (Object.keys(currentErrors).length > 0) {
+			setErrors(currentErrors);
+			return { success: false, errors: currentErrors };
+		}
 
-    const formDataToSend = new FormData();
-    formDataToSend.append("topic_title", String(formData.topic_title));
-    formDataToSend.append(
-      "topic_description",
-      String(formData.topic_description)
-    );
-    if (pdfFile) formDataToSend.append("pdf", pdfFile);
-    if (videoFile) formDataToSend.append("video", videoFile);
+		const formDataToSend = new FormData();
+		formDataToSend.append("topic_title", String(formData.topic_title));
+		formDataToSend.append(
+			"topic_description",
+			String(formData.topic_description)
+		);
+		if (pdfFile) formDataToSend.append("pdf", pdfFile);
+		if (videoFile) formDataToSend.append("video", videoFile);
 
-    setErrors({});
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        `${
-          import.meta.env.VITE_REACT_APP_API_ROOT
-        }/api/topic/create/${courseId}`,
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${user.jwt}`,
-          },
-          body: formDataToSend,
-        }
-      );
-      const json = await response.json();
+		setErrors({});
+		setLoading(true);
+		setError(null);
+		try {
+			const response = await fetch(
+				`${
+					import.meta.env.VITE_REACT_APP_API_ROOT
+				}/api/topic/create/${courseId}`,
+				{
+					method: "POST",
+					headers: {
+						Authorization: `Bearer ${user.jwt}`,
+					},
+					body: formDataToSend,
+				}
+			);
+			const json = await response.json();
 
-      if (response.ok) {
-        setSuccess(true);
-        resetFields();
-      } else {
-        setError(json.error);
-      }
-    } catch (err: any) {
-      setError(err.message || "Failed to create topic");
-    } finally {
-      setLoading(false);
-    }
-  };
+			if (response.ok) {
+				setSuccess(true);
+				resetFields();
+			} else {
+				setError(json.error);
+			}
+		} catch (err: any) {
+			setError(err.message || "Failed to create topic");
+		} finally {
+			setLoading(false);
+		}
+	};
 
-  const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
-    >
-  ) => {
-    const { id, value } = e.target;
-    setFormData((prevState) => ({
-      ...prevState,
-      [id]: value,
-    }));
-  };
+	const handleChange = (
+		e: React.ChangeEvent<
+			HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+		>
+	) => {
+		const { id, value } = e.target;
+		setFormData((prevState) => ({
+			...prevState,
+			[id]: value,
+		}));
+	};
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, files } = e.target;
-    if (files && files.length > 0) {
-      if (id === "pdf") {
-        setPdfFile(files[0]);
-      } else if (id === "video") {
-        setVideoFile(files[0]);
-      }
-    }
-  };
+	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { id, files } = e.target;
+		if (files && files.length > 0) {
+			if (id === "pdf") {
+				setPdfFile(files[0]);
+			} else if (id === "video") {
+				setVideoFile(files[0]);
+			}
+		}
+	};
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    createTopic();
-  };
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		createTopic();
+	};
 
-  const resetFields = () => {
-    setFormData({
-      topic_title: "",
-      topic_description: "",
-      pdf: "",
-      video: "",
-    });
-    setPdfFile(null);
-    setVideoFile(null);
-  };
+	const resetFields = () => {
+		setFormData({
+			topic_title: "",
+			topic_description: "",
+			pdf: "",
+			video: "",
+		});
+		setPdfFile(null);
+		setVideoFile(null);
+	};
 
-  return {
-    createTopic,
-    handleChange,
-    handleFileChange,
-    handleSubmit,
-    setFormData,
-    resetFields,
-    formData,
-    pdfFile,
-    videoFile,
-    loading,
-    error,
-    errors,
-    setErrors,
-    setError,
-    setSuccess,
-    success,
-  };
+	return {
+		createTopic,
+		handleChange,
+		handleFileChange,
+		handleSubmit,
+		setFormData,
+		resetFields,
+		formData,
+		pdfFile,
+		videoFile,
+		loading,
+		error,
+		errors,
+		setErrors,
+		setError,
+		setSuccess,
+		success,
+	};
 };
 
 export default useCreateTopic;
--- src/components/pages/Module/ModulesMainPage.tsx
+++ src/components/pages/Module/ModulesMainPage.tsx
@@ -2,212 +2,217 @@ import { IoMdAdd } from "react-icons/io";
 import useFetchTopics from "../../hooks/module hooks/useFetchTopics";
 import { useParams } from "react-router-dom";
 import useCreateTopic from "../../hooks/module hooks/useCreateTopic";
-import useDeleteTopic from "../../hooks/module hooks/useDeleteTopic";
 
 const ModulesMainPage = () => {
-  const {
-    createTopic,
-    handleChange,
-    handleFileChange,
-    formData,
-    resetFields,
-    errors,
-    error,
-    setErrors,
-    setError,
-    setSuccess,
-    success,
-  } = useCreateTopic();
-  const handleCreateTopic = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await createTopic();
-  };
-  const { courseId } = useParams();
-  const { topics, loading } = useFetchTopics(String(courseId));
+	const {
+		createTopic,
+		handleChange,
+		handleFileChange,
+		formData,
+		resetFields,
+		errors,
+		error,
+		setErrors,
+		setError,
+		setSuccess,
+		success,
+	} = useCreateTopic();
+	const handleCreateTopic = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		await createTopic();
+	};
+	const { courseId } = useParams();
+	const { topics, loading } = useFetchTopics(String(courseId));
 
-  return (
-    <>
-      <div>
-        <button
-          className="btn btn-success font-garet"
-          onClick={() =>
-            (
-              document.getElementById("my_modal_1") as HTMLDialogElement
-            ).showModal()
-          }
-        >
-          <IoMdAdd /> Add Topic
-        </button>
-        <dialog id="my_modal_1" className="modal">
-          <div className="modal-box">
-            <div className="w-full mx-auto">
-              <form
-                onSubmit={handleCreateTopic}
-                className="border border-black p-4 bg-white shadow-md rounded-lg"
-              >
-                <div className="mb-4">
-                  <label
-                    htmlFor="topic_title"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Topic Title
-                  </label>
-                  <input
-                    type="text"
-                    id="topic_title"
-                    placeholder="Enter topic title"
-                    value={formData.topic_title}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg"
-                  />
-                  {errors.topic_title && (
-                    <p className="text-red-500 text-sm">{errors.topic_title}</p>
-                  )}
-                </div>
+	return (
+		<>
+			<div>
+				<button
+					className="btn btn-success font-garet"
+					onClick={() =>
+						(
+							document.getElementById("my_modal_1") as HTMLDialogElement
+						).showModal()
+					}
+				>
+					<IoMdAdd /> Add Topic
+				</button>
+				<dialog
+					id="my_modal_1"
+					className="modal"
+				>
+					<div className="modal-box">
+						<div className="w-full mx-auto">
+							<form
+								onSubmit={handleCreateTopic}
+								className="border border-black p-4 bg-white shadow-md rounded-lg"
+							>
+								<div className="mb-4">
+									<label
+										htmlFor="topic_title"
+										className="block text-gray-700 font-bold mb-2"
+									>
+										Topic Title
+									</label>
+									<input
+										type="text"
+										id="topic_title"
+										placeholder="Enter topic title"
+										value={formData.topic_title}
+										onChange={handleChange}
+										className="w-full px-3 py-2 border border-gray-300 rounded-lg"
+									/>
+									{errors.topic_title && (
+										<p className="text-red-500 text-sm">{errors.topic_title}</p>
+									)}
+								</div>
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="topic_description"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Topic Description
-                  </label>
-                  <textarea
-                    id="topic_description"
-                    placeholder="Enter topic description"
-                    value={formData.topic_description}
-                    onChange={handleChange}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg"
-                  />
+								<div className="mb-4">
+									<label
+										htmlFor="topic_description"
+										className="block text-gray-700 font-bold mb-2"
+									>
+										Topic Description
+									</label>
+									<textarea
+										id="topic_description"
+										placeholder="Enter topic description"
+										value={formData.topic_description}
+										onChange={handleChange}
+										className="w-full px-3 py-2 border border-gray-300 rounded-lg"
+									/>
 
-                  {errors.topic_description && (
-                    <p className="text-red-500 text-sm">
-                      {errors.topic_description}
-                    </p>
-                  )}
-                </div>
+									{errors.topic_description && (
+										<p className="text-red-500 text-sm">
+											{errors.topic_description}
+										</p>
+									)}
+								</div>
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="pdf"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    PDF File
-                  </label>
-                  <input
-                    type="file"
-                    id="pdf"
-                    name="pdf"
-                    onChange={handleFileChange}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg"
-                  />
-                </div>
+								<div className="mb-4">
+									<label
+										htmlFor="pdf"
+										className="block text-gray-700 font-bold mb-2"
+									>
+										PDF File
+									</label>
+									<input
+										type="file"
+										id="pdf"
+										name="pdf"
+										onChange={handleFileChange}
+										className="w-full px-3 py-2 border border-gray-300 rounded-lg"
+									/>
+								</div>
 
-                <div className="mb-4">
-                  <label
-                    htmlFor="video"
-                    className="block text-gray-700 font-bold mb-2"
-                  >
-                    Video File
-                  </label>
-                  <input
-                    type="file"
-                    id="video"
-                    name="video"
-                    onChange={handleFileChange}
-                    className="w-full px-3 py-2 border border-gray-300 rounded-lg"
-                  />
-                  {errors.video && (
-                    <p className="text-red-500 text-sm">{errors.video}</p>
-                  )}
-                </div>
+								<div className="mb-4">
+									<label
+										htmlFor="video"
+										className="block text-gray-700 font-bold mb-2"
+									>
+										Video File
+									</label>
+									<input
+										type="file"
+										id="video"
+										name="video"
+										onChange={handleFileChange}
+										className="w-full px-3 py-2 border border-gray-300 rounded-lg"
+									/>
+									{errors.video && (
+										<p className="text-red-500 text-sm">{errors.video}</p>
+									)}
+								</div>
 
-                <div className="flex justify-between">
-                  <button
-                    disabled={loading}
-                    type="submit"
-                    className="btn btn-info hover:bg-neutral text-white font-bold py-2 px-4 rounded-lg"
-                    // disabled={loading}
-                  >
-                    Create Topic
-                  </button>
-                </div>
-                {error && (
-                  <p className="text-white text-bold mt-2 p-4 bg-red-400 text-center">
-                    {error}
-                  </p>
-                )}
-                {success && (
-                  <p className="text-white text-bold mt-2 p-4 bg-green-400 text-center">
-                    Topic created successfully!
-                  </p>
-                )}
-              </form>
-              <div className="modal-action">
-                <form method="dialog">
-                  {/* if there is a button, it will close the modal */}
-                  <button
-                    onClick={() => {
-                      setError(null);
-                      setErrors({});
-                      resetFields();
-                      setSuccess(false);
-                    }}
-                    className="btn"
-                  >
-                    Close
-                  </button>
-                  {errors.video && (
-                    <p className="text-red-500 text-sm">{errors.video}</p>
-                  )}
-                </form>
-              </div>
-            </div>
-          </div>
-        </dialog>
-      </div>
-      <div className="w-1/2 overflow-x-auto bg-white rounded-box mt-4">
-        {loading ? (
-          <div className="text-black font-garet text-center text-4xl m-4 bg-white rounded-xl p-4">
-            Loading...
-          </div>
-        ) : error ? (
-          <div className="text-black font-garet text-center text-4xl m-4 bg-white rounded-xl p-4">
-            {error}
-          </div>
-        ) : !topics?.length ? (
-          <div className="text-black font-garet text-center text-4xl m-4 bg-white rounded-xl p-4">
-            No topics available
-          </div>
-        ) : (
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Module Name</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {topics.map((topic, index) => (
-                <tr key={topic._id}>
-                  <th>{index + 1}</th>
-                  <td>{topic.topic_title}</td>
-                  <td>{topic.topic_description}</td>
-                  <td>
-                    <a href={`/${topic.topic_title}`} className="btn btn-ghost">
-                      More Details
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
-    </>
-  );
+								<div className="flex justify-between">
+									<button
+										disabled={loading}
+										type="submit"
+										className="btn btn-info hover:bg-neutral text-white font-bold py-2 px-4 rounded-lg"
+										// disabled={loading}
+									>
+										Create Topic
+									</button>
+								</div>
+								{error && (
+									<p className="text-white text-bold mt-2 p-4 bg-red-400 text-center">
+										{error}
+									</p>
+								)}
+								{success && (
+									<p className="text-white text-bold mt-2 p-4 bg-green-400 text-center">
+										Topic created successfully!
+									</p>
+								)}
+							</form>
+							<div className="modal-action">
+								<form method="dialog">
+									{/* if there is a button, it will close the modal */}
+									<button
+										onClick={() => {
+											setError(null);
+											setErrors({});
+											resetFields();
+											setSuccess(false);
+										}}
+										className="btn"
+									>
+										Close
+									</button>
+									{errors.video && (
+										<p className="text-red-500 text-sm">{errors.video}</p>
+									)}
+								</form>
+							</div>
+						</div>
+					</div>
+				</dialog>
+			</div>
+			<div className="w-1/2 overflow-x-auto bg-white rounded-box mt-4">
+				{loading ? (
+					<div className="text-black font-garet text-center text-4xl m-4 bg-white rounded-xl p-4">
+						Loading...
+					</div>
+				) : error ? (
+					<div className="text-black font-garet text-center text-4xl m-4 bg-white rounded-xl p-4">
+						{error}
+					</div>
+				) : !topics?.length ? (
+					<div className="text-black font-garet text-center text-4xl m-4 bg-white rounded-xl p-4">
+						No topics available
+					</div>
+				) : (
+					<table className="table">
+						{/* head */}
+						<thead>
+							<tr>
+								<th></th>
+								<th>Module Name</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							{topics.map((topic, index) => (
+								<tr key={topic._id}>
+									<th>{index + 1}</th>
+									<td>{topic.topic_title}</td>
+									<td>{topic.topic_description}</td>
+									<td>
+										<a
+											href={`/${topic.topic_title}`}
+											className="btn btn-ghost"
+										>
+											More Details
+										</a>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				)}
+			</div>
+		</>
+	);
 };
 
 export default ModulesMainPage;
